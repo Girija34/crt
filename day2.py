9925c0b5a0896c3858672f8df89ce874e6a27964
Girija34 <113780850+Girija34@users.noreply.github.com>
2023-01-31 15:24:34 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,59 @@
+for i in range(1,151):
+    if i%10==0:
+        print(i)
+
+
+
+for i in range(1,6,2):
+    print(i*5)
+
+
+for i in range(1,30):
+    if(i%2==1,i<=5):
+        print(i*5)
+
+
+for i in range(-10,2,2):
+    print(i)
+
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    
+
+
+
+
+for i in range(97,123,2):
+    for j in range(97,i+1):
+        print(chr(i),end=" ")
+    print("/n")
+
+for i in range(1,11):
+    print(i,"*10=",i*10)
+
+
+i=1
+while(i<=5):
+    j=1
+    while(j<=i):
+        print(i,end=" ")
+        j=j+1
+    print("/n")
+    i=i+1
+
+
+
+
+
+    
+
+
+
+
+
+    
+        
+    
+
